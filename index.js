da5e42dbaf12c2eb208d2285279d9e1c34f383ce
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-22 23:03:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const div = document.querySelector(div);
-function addingEventListener() {
-    const input = document.getElementById('input');
+// const input = document.getElementById('button');
+// input.addEventListener('click', () => alert('I was clicked!'));
+//When we want to reuse the callback function, it is good practice to create a separate function
+// const input = document.getElementById('button');
+// 
+// function clickAlert(){
+//   alert('I was clicked!');
+// }
 
-function clickAlert() {
-  alert('I was clicked!');
-}
-input.addEventListener('click', clickAlert);
+// input.addEventListener('click', clickAlert);
+
+// let input = document.querySelector('#button');
+// input.addEventListener('click', () => alert('clicked'));
+
+let input = document.getElementById('button');
+
+function addingEventListener() {
+  alert('I was clicked!')
+  input.addEventListener('click', addingEventListener);
 }
